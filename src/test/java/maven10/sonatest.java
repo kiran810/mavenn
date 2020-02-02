b4b2a6bc37ab59f29c97ebe9798a330e00a5721e
kiran <kiraneng.kiran@gmail.com>
2020-02-02 21:59:09 +0530

    sending maven10

---
+++ src/test/java/maven10/sonatest.java
@@ -0,0 +1,19 @@
+package maven10;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class sonatest {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
